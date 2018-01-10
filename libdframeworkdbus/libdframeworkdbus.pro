b577a8be9cbb3934fff180fed8070c1248e60ed4
石博文 <sbw@sbw.so>
2018-01-10 10:53:00 +0800

    add chrome tabs manage extension
    Change-Id: I5eb49bd6d664bf6770fba8f000e3f883d0393f33

--- libdframeworkdbus/libdframeworkdbus.pro
+++ libdframeworkdbus/libdframeworkdbus.pro
@@ -37,7 +37,8 @@ HEADERS += \
     types/sinkinfolist.h \
     types/qvariantmap.h \
     types/appscgroupinfo.h \
-    types/appscgroupinfolist.h
+    types/appscgroupinfolist.h \
+    types/chrometablist.h
 
 SOURCES += \
     types/zoneinfo.cpp \
@@ -70,7 +71,8 @@ SOURCES += \
     types/linkinfolist.cpp \
     types/sinkinfolist.cpp \
     types/qvariantmap.cpp \
-    types/appscgroupinfolist.cpp
+    types/appscgroupinfolist.cpp \
+    types/chrometablist.cpp
 
 !system(python $$PWD/generate_code.py): error("Failed to generate code")
 include(generated/generated.pri)
---
+++ libdframeworkdbus/types/chrometablist.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2011 ~ 2017 Deepin Technology Co., Ltd.
+ *
+ * Author:     sbw <sbw@sbw.so>
+ *             kirigaya <kirigaya@mkacg.com>
+ *             Hualet <mr.asianwang@gmail.com>
+ *
+ * Maintainer: sbw <sbw@sbw.so>
+ *             kirigaya <kirigaya@mkacg.com>
+ *             Hualet <mr.asianwang@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CHROMETABS_H
+#define CHROMETABS_H
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QMap>
+#include <QtCore/QString>
+#include <QDBusArgument>
+#include <QDBusMetaType>
+
+struct ChromeTabInfo {
+    qint32 id;
+    quint64 memory;
+    QString title;
+
+    bool operator ==(const ChromeTabInfo& info);
+};
+
+typedef QList<ChromeTabInfo> ChromeTabList;
+
+Q_DECLARE_METATYPE(ChromeTabInfo)
+Q_DECLARE_METATYPE(ChromeTabList)
+
+QDBusArgument &operator<<(QDBusArgument &arg, const ChromeTabInfo &info);
+const QDBusArgument &operator>>(const QDBusArgument &arg, ChromeTabInfo &info);
+
+void registerChromeTabListMetaType();
+
+#endif // CHROMETABS_H
