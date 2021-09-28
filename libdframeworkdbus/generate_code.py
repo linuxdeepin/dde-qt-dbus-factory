68c186a8a712de84c9635c9af0b8ecdb027fe9c7
Clay Stan <liuyong@uniontech.com>
2021-09-28 14:21:34 +0800

    chore: Migrate to python3
    Migrate to python3
    ref:
    https://salsa.debian.org/pkg-deepin-team/dde-qt-dbus-factory/-/blob/master/debian/patches/0001-pro-Explicitly-use-python3-instead-of-python.patch
    https://github.com/linuxdeepin/dde-qt-dbus-factory/pull/16
    
    Log: Migrate to python3
    Issue: https://github.com/linuxdeepin/dde-qt-dbus-factory/issues/2
    Change-Id: Ic4221a2b9aced0bf5fdc256ef443c50b15c6a24d

--- libdframeworkdbus/generate_code.py
+++ libdframeworkdbus/generate_code.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function
 import os
 import glob
 import subprocess
@@ -52,8 +51,8 @@ def main():
 
         print("generating the pri file...")
         pwdfy = lambda x: "$$PWD/%s" % x
-        sources = map(pwdfy, sorted(glob.glob1(generated_dir, "*.cpp")))
-        headers = map(pwdfy, sorted(glob.glob1(generated_dir, "*.h")))
+        sources = list(map(pwdfy, sorted(glob.glob1(generated_dir, "*.cpp"))))
+        headers = list(map(pwdfy, sorted(glob.glob1(generated_dir, "*.h"))))
         pri.write("HEADERS += %s\n" % " ".join(headers))
         pri.write("SOURCES += %s" % " ".join(sources))
         print("done.")
