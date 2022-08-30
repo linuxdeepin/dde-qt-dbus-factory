cc2a09558b78be6a975e4c1fff38bc4eaa814a59
shaojun <wangshaojun@uniontech.com>
2022-08-30 17:05:30 +0800

    chore: adapt license and copyright
    Modify project to pass the REUSE check
    
    Log: Modify project to pass the REUSE check
    Task: https://pms.uniontech.com/task-view-185215.html
    Change-Id: Ia6cca51e7aceffc15c90ab6149dacaf746f5d6ad

--- libdframeworkdbus/types/defenderprocinfo.cpp
+++ libdframeworkdbus/types/defenderprocinfo.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "defenderprocinfo.h"
 
 bool DefenderProcInfo::operator!=(const DefenderProcInfo &procInfo)
--- libdframeworkdbus/types/windowinfomap.h
+++ libdframeworkdbus/types/windowinfomap.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef WINDOWINFOLIST_H
 #define WINDOWINFOLIST_H
 
